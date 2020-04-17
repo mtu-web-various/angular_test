515d0949d9483cf6074066188061ac17ee0ade2c
Talha <talhamucar@gmail.com>
2020-04-17 00:21:02 -0400

    bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasklistComponent } from './tasks/tasklist/tasklist.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasklistComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
